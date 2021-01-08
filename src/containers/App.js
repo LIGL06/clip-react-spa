a55cea44bcc14987247962cb57c9efb6bc924b85
Iván García <luis.garcialuna@outlook.com>
2021-01-08 14:06:16 -0600

    Fix some Router issues

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import '../styles/App.css';
 import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" component={Dashboard} />
-    </Switch>
+    <Router>
+      <Switch>
+        <Route path="/" component={Dashboard} />
+      </Switch>
+    </Router>
   );
 }
 
