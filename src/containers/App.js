c458c0f2e1f7ec6fc0103ae93e668c019b68c316
Ivan Garcia <luis.garcialuna@outlook.com>
2021-01-07 18:48:13 -0600

    Basic structure

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <h1>Clip OpenPay App</h1>
+  );
+}
+
+export default App;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../containers/App';
 
-test('renders learn react link', () => {
+test('renders Clip OpenPay App text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Clip OpenPay App/i);
   expect(linkElement).toBeInTheDocument();
 });
